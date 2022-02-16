7d36f4e95efa161fa022e7f252f32c611c2fd84e
Flame0510 <micheletornello5@gmail.com>
2022-02-16 15:09:40 +0100

    fix(app): socket server

--- dist/app.js
+++ dist/app.js
@@ -8,16 +8,16 @@ var express_1 = __importDefault(require("express"));
 var http_1 = require("http");
 var cors_1 = __importDefault(require("cors"));
 var socket_io_1 = require("socket.io");
-exports.io = new socket_io_1.Server({
+var app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({ origin: true }));
+var httpServer = (0, http_1.createServer)(app);
+exports.io = new socket_io_1.Server(httpServer, {
     cors: {
         origin: "http://localhost:8100",
     },
 });
 exports.io.on("connection", function (socket) { return socket.join("match1"); });
-var app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: true }));
-var httpServer = (0, http_1.createServer)(app);
 var auth_1 = __importDefault(require("./routes/auth"));
 var matches_1 = __importDefault(require("./routes/matches"));
 app.use("/auth", auth_1.default);
--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,19 @@ import cors from "cors";
 
 import { Server } from "socket.io";
 
-export const io = new Server({
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const httpServer = createServer(app);
+
+export const io = new Server(httpServer, {
   cors: {
     origin: "http://localhost:8100",
   },
 });
 
 io.on("connection", (socket) => socket.join("match1"));
-
-const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
-
-const httpServer = createServer(app);
 
 import auth from "./routes/auth";
 import matches from "./routes/matches";
